f50272220cf756670ac45206a1185b72a194812f
nikihsah <85619792+nikihsah@users.noreply.github.com>
2021-11-29 14:43:59 +0300

    Update 7lab.cpp

--- 7/7lab/7lab/7lab.cpp
+++ 7/7lab/7lab/7lab.cpp
@@ -167,6 +167,61 @@ public:
 
 };
 
+template <typename T> class Stack
+{
+    T* stackPtr;
+    int size;
+    T top;
+
+public:
+    Stack(int = 10);
+
+    ~Stack();
+
+    bool push(const T);
+    bool pop();
+    void printStack();
+};
+
+template <typename T> Stack<T>::Stack(int s)
+{
+    size = s > 0 ? s : 10;
+    stackPtr = new T[size];
+    top = -1; // çíà÷åíèå -1 ãîâîðèò î òîì, ÷òî ñòåê ïóñò
+}
+
+template <typename T> Stack<T>::~Stack()
+{
+    delete[] stackPtr;
+}
+
+template <typename T> bool Stack<T>::push(const T value)
+{
+    if (top == size - 1)
+        return false;
+
+    top++;
+    stackPtr[top] = value;
+
+    return true;
+}
+
+template <typename T> bool Stack<T>::pop()
+{
+    if (top == -1)
+        return false;
+
+    stackPtr[top] = 0;
+    top--;
+
+    return true;
+}
+
+template <typename T> void Stack<T>::printStack()
+{
+    for (int ix = size - 1; ix >= 0; ix--)
+        cout << "|" << setw(4) << stackPtr[ix] << endl;
+}
 
 int main()
 {
